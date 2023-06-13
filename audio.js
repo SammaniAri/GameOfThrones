fe38bb5b35695188ffcdfdf2036fa8c385ab2b5c
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-06-13 22:39:38 +0200

    Adding audio

---
+++ audio.js
@@ -0,0 +1,12 @@
+/** @format */
+let audio = new Audio(
+	"http://soundfxcenter.com/television/game-of-thrones/8d82b5_Game_of_Thrones_Theme_Song.mp3"
+);
+
+function playAudio() {
+	audio.play();
+}
+
+function pauseAudio() {
+	audio.pause();
+}
